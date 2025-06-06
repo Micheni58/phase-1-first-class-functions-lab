2e147999e4c33d91f0ee7b12907e276b941cd0a0
Micheni58 <michenicaleb@gmail.com>
2025-06-06 12:01:00 +0300

    Modifies index.js

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = ()=>{
+    return drivers.slice(0,2);
+}
+console.log(returnFirstTwoDrivers());
+
+const returnLastTwoDrivers = ()=>{
+    return drivers.slice(-2);
+}
+console.log(returnLastTwoDrivers());
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+console.log(selectingDrivers[0]());
+console.log(selectingDrivers[1]());
+
+
+
+function createFareMultiplier(multiplier){
+    return function(fare){
+        return fare * multiplier;
+    }
+}
+const fareDoubler = createFareMultiplier(2);
+console.log(fareDoubler(10));
+
+const fareTripler = createFareMultiplier(3);
+console.log(fareTripler(10));
+
+const fareQuintupler = createFareMultiplier(5);
+console.log(fareQuintupler(5)); 
+
+const selectDifferentDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+ console.log(selectDifferentDrivers[0]);
+ console.log(selectDifferentDrivers[1]);
